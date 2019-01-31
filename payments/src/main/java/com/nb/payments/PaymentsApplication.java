d0d7486292bd73dcd52dadcfb78e81d651edffa4
nb <nazik.lv@gmail.com>
2019-01-31 09:01:30 +0200

    added payments stub

---
+++ payments/src/main/java/com/nb/payments/PaymentsApplication.java
@@ -0,0 +1,14 @@
+package com.nb.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentsApplication.class, args);
+  }
+
+}
+
---
+++ payments/src/test/java/com/nb/payments/PaymentsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nb.payments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaymentsApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
