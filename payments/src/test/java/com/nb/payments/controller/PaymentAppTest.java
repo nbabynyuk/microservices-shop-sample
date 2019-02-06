7f9508ffd62099c0c3e4a18130dcd93fea88b648
nb <nazik.lv@gmail.com>
2019-02-06 09:26:14 +0200

    Refactored payments controller

--- payments/src/test/java/com/nb/payments/controller/PaymentAppTest.java
+++ payments/src/test/java/com/nb/payments/controller/PaymentAppTest.java
@@ -1,10 +1,17 @@
-package com.nb.payments.functional;
+package com.nb.payments.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.nb.payments.entity.PaymentData;
+import com.nb.payments.functional.PaymentController;
+import com.nb.payments.repo.PaymentRepo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -13,15 +20,23 @@ import reactor.core.publisher.Mono;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest(PaymentController.class)
-//@Import(EmbeddedMongoConfig.class)
 public class PaymentAppTest {
+
+  @MockBean
+  private PaymentRepo paymentRepo;
 
   @Autowired
   private WebTestClient webTestClient;
 
-
   @Test
   public void testProcessPayment() throws Exception {
+
+    when(paymentRepo.insert(any(PaymentData.class))).
+        then((Answer<Mono<PaymentData>>) invocationOnMock -> {
+          PaymentData pd = new PaymentData();
+          pd.setUuid("xxx-111");
+          return Mono.just(pd);
+        });
 
     String value = "{ "
         + " \"paymentFrom\":{"
@@ -34,7 +49,7 @@ public class PaymentAppTest {
         + " }";
 
      webTestClient.post()
-        .uri("/payments")
+        .uri("/api/payments")
         .contentType(MediaType.APPLICATION_JSON_UTF8)
         .accept(MediaType.APPLICATION_JSON_UTF8)
         .body(Mono.just(value), String.class)
