a2bde336661906058b066c4d530b75bc7dd29856
nb <nazik.lv@gmail.com>
2020-08-29 09:35:15 +0300

    Removed eureka server

--- payments/src/main/java/com/nb/payments/PaymentsApplication.java
+++ payments/src/main/java/com/nb/payments/PaymentsApplication.java
@@ -4,14 +4,12 @@ import com.nb.payments.functional.PaymentHandlerFunctions;
 import com.nb.payments.repo.PaymentRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class PaymentsApplication {
 
   public static void main(String[] args) {
--- product-catalog/src/main/java/com/nb/products/ProductCatalogApp.java
+++ product-catalog/src/main/java/com/nb/products/ProductCatalogApp.java
@@ -6,10 +6,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ProductCatalogApp implements ApplicationRunner {
 
   @Autowired
