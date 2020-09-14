e91667eca1f9a01aa451ff214f126ff7427c1f7a
nazarb <nazik.lv@gmail.com>
2020-09-14 11:11:07 +0300

    Re-structured maven dependencies, addes skeleton for gateway app

---
+++ api-gateway/src/main/java/com/nb/ApiGatewayApp.java
@@ -0,0 +1,13 @@
+package com.nb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApp.class, args);
+    }
+
+}
